8bf1b92102cb9b77e10dc1462f73a6db38d63e32
Margus Pala <margus.pala@gmail.com>
2018-11-05 12:15:16 +0200

    Return shorter name companies in front to make sure they are included in results

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -7,33 +7,37 @@ use DebugBar\DebugBar;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
-class ApiController extends Controller {
+class ApiController extends Controller
+{
 
-	public function getCompanyByName( Request $request ) {
+    public function getCompanyByName(Request $request)
+    {
+        $companyName = $request->name;
+        if (strlen($companyName) < 3) {
+            return response()->json([
+                "status"  => "error",
+                "message" => "Minimum 3 characters needed"
+            ], 400);
+        }
+        $companyName = str_replace(" ", " ", $companyName);
+        info("Looking for company: $companyName");
 
-		$companyName = $request->name;
-		if ( strlen( $companyName ) < 3 ) {
-			return response()->json( [
-				"status"  => "error",
-				"message" => "Minimum 3 characters needed"
-			], 400 );
-		}
-		$companyName = str_replace( " ", " ", $companyName );
-		info( "Looking for company: $companyName" );
+        //If better performance is needed without partial name matches then this condition can be used
+        //whereRaw( 'MATCH(nimi_lower) AGAINST(?)', $companyName )
+        $companies = Company::where("nimi_lower", 'like', "%$companyName%")
+                            ->get();
 
-		//If better performance is needed without partial name matches then this condition can be used
-		//whereRaw( 'MATCH(nimi_lower) AGAINST(?)', $companyName )
-		$companies = Company::where( "nimi_lower", 'like', "%$companyName%" )
-		                    ->limit( 5 )
-		                    ->get();
+        $companies = $companies->sortBy(function ($company) {
+            return mb_strlen($company->nimi_lower);
+        })->values()->take(5);
 
-		$response = response()->json( $companies );
+        $response = response()->json($companies);
 
-		//Support also jsonp responses
-		if ( $request->callback ) {
-			$response->withCallback( $request->callback );
-		}
+        //Support also jsonp responses
+        if ($request->callback) {
+            $response->withCallback($request->callback);
+        }
 
-		return $response;
-	}
+        return $response;
+    }
 }
